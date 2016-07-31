608f52c5956cd92771604e3a365958ce30ebf041
Glenn Hermans <glhermans@gmail.com>
2016-07-31 06:00:40 +0200

    Updated authentication routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+
 
 Route::auth();
+Route::get('/register', 'HomeController@index');
 
 Route::get('/home', 'HomeController@index');
