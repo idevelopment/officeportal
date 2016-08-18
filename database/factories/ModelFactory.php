b81705d4be0ca1ab74bab05742f4c202cfeb3255
Tjoosten <Tjoosten3@gmail.com>
2016-08-19 00:14:01 +0200

    Fix Migration issues | TravisCi

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,7 +13,14 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
+        'fname' => $faker->firstName,
         'name' => $faker->name,
+        'address' => $faker->address,
+        'zipcode' => $faker->citySuffix,
+        'city' => $faker->city,
+        'country' => $faker->country,
+        'phone' => $faker->phoneNumber,
+        'mobile' => $faker->phoneNumber,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
