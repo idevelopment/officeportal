7dc26252a2a9e4649b4857440dbf3cad4385fd43
Tjoosten <Tjoosten3@gmail.com>
2016-08-18 16:43:37 +0200

    Working update on departmentsController

--- app/Http/Controllers/DepartmentsController.php
+++ app/Http/Controllers/DepartmentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Departments;
+use App\Http\Requests\DepartmentValidator;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -13,22 +14,78 @@ use App\Http\Requests;
  */
 class DepartmentsController extends Controller
 {
+    // TODO: Write tests for this department.
+    // TODO: Fill in the validator.
+    // TODO: Create department member relationship.
+    // TODO: Register routes.
+
     /**
      * DepartmentsController constructor.
      */
     public function __construct()
     {
         $this->middleware('auth');
+        // $this->middleware('lang');
     }
 
     /**
      * Show the departments.
      *
+     * @url    GET|HEAD:
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         return view('departments/index');
+    }
+
+    /**
+     * Register from for a new department.
+     *
+     * @url    GET|HEAD:
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function register()
+    {
+        return view();
+    }
+
+    /**
+     * Store a new department in the database.
+     *
+     * @url
+     * @param  DepartmentValidator $input
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(DepartmentValidator $input)
+    {
+        return redirect()->back();
+    }
+
+    /**
+     * Update view for a specific department.
+     *
+     * @url    GET|HEAD:
+     * @param  int $id the id off the department in the database.
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $data['department'] = Departments::with('managers')->find($id)
+        return view('', $data);
+    }
+
+    /**
+     * Update a department in the database.
+     *
+     * @url    POST:
+     * @param  int $id the id off the department in the database.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update($id)
+    {
+        session()->flash('The department has been updated');
+        return redirect()->back();
     }
 
     /**
